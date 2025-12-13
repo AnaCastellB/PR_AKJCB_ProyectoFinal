85300d7a2be9e5fe25b1bae29d2970c34cbd6406
AnaCastellB <anakarenjacqueline@gmail.com>
2025-12-12 23:26:02 -0600

    Creacion del modelo videojuego

---
+++ src/main/java/org/example/model/Videojuego.java
@@ -0,0 +1,45 @@
+package org.example.model;
+
+public class Videojuego {
+
+    private int id;
+    private String titulo;
+    private String consola;
+    private double precio;
+    private int stock;
+
+    public Videojuego(int id, String titulo, String consola, double precio, int stock) {
+        this.id = id;
+        this.titulo = titulo;
+        this.consola = consola;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getConsola() {
+        return consola;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", consola='" + consola + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+}
